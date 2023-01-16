ca19bdfedef9687c53306b134ca39e204514aadf
nameko710 <118578225+nameko710@users.noreply.github.com>
2023-01-16 12:33:26 +0900

    食品登録機能の作成

--- app/controllers/fridges_controller.rb
+++ app/controllers/fridges_controller.rb
@@ -1,2 +1,24 @@
 class FridgesController < ApplicationController
+  def index
+    @fridges = Fridge.all
+  end
+
+  def new
+    @fridges = Fridge.new
+  end 
+
+  def create
+    @fridge = Fridge.new(fridge_params)
+    if @fridge.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+
+  private
+  def fridge_params
+    params.require(:fridge).permit(:food, :amount, :unit, :price).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'fridges#index'
+  resources :fridges, only: [:index, :new, :create]
 end
