19f1f247900c3a1f222d37ee8b89477767338c40
nameko710 <118578225+nameko710@users.noreply.github.com>
2023-01-17 21:25:29 +0900

    バリデーションの設定

--- app/models/fridge.rb
+++ app/models/fridge.rb
@@ -1,3 +1,11 @@
 class Fridge < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :availability, inclusion: { in: [true] }
+    validates :food
+    validates :amount, numericality: {greater_than: 0}
+    validates :unit
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 0 }
+   end
 end
