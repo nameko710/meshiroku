cb4190b67f20a67b95bc210a87773c94d9d82831
nameko710 <118578225+nameko710@users.noreply.github.com>
2023-01-17 21:54:58 +0900

    fridgeモデルの単体テストを作成

--- spec/factories/fridges.rb
+++ spec/factories/fridges.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :fridge do
-    
+    availability   {"true"}
+    food           {Faker::Name.initials(number: 2)}
+    amount         {Faker::Number.between(from: 1, to: 100)}
+    unit           {Faker::Name.initials(number: 2)}
+    price          {Faker::Number.between(from: 1, to: 1000)}
+    association :user
   end
 end
