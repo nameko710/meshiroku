65863212fc57dd9b96946685a7808ca484643a9c
nameko710 <118578225+nameko710@users.noreply.github.com>
2023-01-18 10:35:10 +0900

    eatモデルとコントローラーを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_16_023748) do
+ActiveRecord::Schema.define(version: 2023_01_18_012840) do
+
+  create_table "eats", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "price", null: false
+    t.text "comment"
+    t.date "string", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_eats_on_user_id"
+  end
 
   create_table "fridges", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.boolean "availability"
@@ -37,5 +48,6 @@ ActiveRecord::Schema.define(version: 2023_01_16_023748) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "eats", "users"
   add_foreign_key "fridges", "users"
 end
