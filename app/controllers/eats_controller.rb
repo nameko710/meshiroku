1f472b20608e4658ce4454424d37ece28f15591f
nameko710 <118578225+nameko710@users.noreply.github.com>
2023-01-27 10:36:46 +0900

    食事記録機能のビューを作成

--- app/controllers/eats_controller.rb
+++ app/controllers/eats_controller.rb
@@ -5,20 +5,29 @@ class EatsController < ApplicationController
   
   def new
     @eat = Eat.new
+    @form = Form::FridgeCollection.new
+    @fridges = current_user.fridges
   end
 
   def create
-    @eat = Eat.new(eat_params)
-    if @eat.save
-      redirect_to root_path
+     @eat = Eat.new(eat_params)
+     @form = Form::FridgeCollection.new(fridge_collection_params)
+    if @eat.save && @form.save
+      redirect_to action: :index
     else
-      render :new
+    render :new
     end
   end
 
   private
+    def eat_params
+      params.require(:form_fridge_collection)
+      .permit(:title, :image, :price, :comment, :date).merge(user_id: current_user.id)
+    end
 
-  def eat_params
-    params.require(:eat).permit(:image, :title, :price, :comment, :date).merge(user_id: current_user.id)
-  end
+    def fridge_collection_params
+      params.require(:form_fridge_collection)
+      .permit(fridges_attributes: [:id, :amount, :price]).merge(user_id: current_user.id)
+    end
+
 end
