fa6d35cf897ca59da4311788d53aeff33c44334b
nameko710 <118578225+nameko710@users.noreply.github.com>
2023-01-31 11:51:32 +0900

    食事記録と食材更新機能の作成

--- app/models/eat.rb
+++ app/models/eat.rb
@@ -1,4 +1,11 @@
 class Eat < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :image
+    validates :date
+    validates :price
+  end
 end
--- app/models/form/fridge_collection.rb
+++ app/models/form/fridge_collection.rb
@@ -1,6 +1,6 @@
 class Form::FridgeCollection < Form::Base
   FORM_COUNT = 1
-  attr_accessor :fridges, :user_id
+  attr_accessor :fridges, :user_id, :eats
 
   def initialize(attributes = {})
     super attributes
@@ -16,6 +16,19 @@ class Form::FridgeCollection < Form::Base
       self.fridges.map do |fridge|
         fridge["user_id"] = user_id
         fridge.save
+      end
+    end
+      return true
+    rescue => e
+      return false
+  end
+
+  def update
+    Fridge.transaction do
+      self.fridges.map do |fridge|
+          before_fridge = Fridge.find(fridge.id)
+          after_amount = before_fridge.amount - fridge.amount
+          before_fridge.update!(amount: after_amount)
       end
     end
       return true
