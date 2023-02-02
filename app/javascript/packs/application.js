2044e87958b8ae48d8efd1b4cabfc0f919027633
nameko710 <118578225+nameko710@users.noreply.github.com>
2023-02-02 17:40:46 +0900

    gemの導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require("chartkick")
+require("chart.js")
 require("../add-form")
 require("../eat")
 
